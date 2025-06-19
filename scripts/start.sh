ba92a3f8e8ed4fcd97587d4b9a5ddc7e1b52f831
Habib Qayoom <165273464+HabibQayoom@users.noreply.github.com>
2025-06-19 22:19:54 +0500

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/app
+aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 580420849247.dkr.ecr.ap-southeast-2.amazonaws.com
 docker compose up -d --build
