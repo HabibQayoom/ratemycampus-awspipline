328a3597399dad8b55c2da02d5baf86bfd25fb6c
Habib Qayoom <165273464+HabibQayoom@users.noreply.github.com>
2025-06-19 22:28:53 +0500

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-cd /home/ubuntu/app
+echo "Navigating to app directory..."
+cd /home/ubuntu/ratemycampus
 aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 580420849247.dkr.ecr.ap-southeast-2.amazonaws.com
-docker compose up -d --build
+
+echo "Navigating to app directory..."
+cd /home/ubuntu/ratemycampus
+
+echo "Starting Docker containers using Docker Compose..."
+docker compose down || true  # Stop if already running
+docker compose pull           # Pull latest ECR images (if defined in docker-compose)
+docker compose up -d         # Run containers in detached mode
+
+echo "Deployment complete!"
